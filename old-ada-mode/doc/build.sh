b3a47057776ef53261c229af94ae835e4cbec11b
james dyer <captainflasmr@gmail.com>
2026-04-20 08:02:22 +0100

    removed old ada mode

--- old-ada-mode/doc/build.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-texi2any -o ada-mode.info --no-split ada-mode.texi
-texi2any --html -o ada-mode.html --no-split ada-mode.texi
--- old-ada-mode/doc/clean.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-rm ada-mode.aux ada-mode.fn ada-mode.log ada-mode.toc
